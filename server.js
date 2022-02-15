4ef1846ee04b0316a89d42fdaaa3d745ecfa3d74
masonjar <mason.whitaker79@gmail.com>
2022-02-15 08:12:57 -0800

    Finished St step on of lab08

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const weather = require("./data/weather.json");
+// const weather = require("./data/weather.json");
+const axios = require("axios");
 
 const app = express();
 app.use(cors());
@@ -14,17 +15,21 @@ app.get("/", (request, response) => {
   response.send("This is the Mighty Home Route, bow or die.");
 });
 
-app.get("/weather", (request, response) => {
+app.get("/weather", async (request, response) => {
   let lat = request.query.lat;
   let lon = request.query.lon;
   let searchQuery = request.query.searchQuery;
 
-  const city = weather.find(
-    (cityObj) => cityObj.city_name.toLowerCase() === searchQuery.toLowerCase()
-  );
-    console.log(city);
+  // const city = weather.find(
+  //   (cityObj) => cityObj.city_name.toLowerCase() === searchQuery.toLowerCase()
+  // );
+    // console.log(city);
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&days=7&lat=${lat}&lon=${lon}`
+  console.log(url);
   try {
-    const weatherArray = city.data.map((day) => new Forecast(day));
+    const weatherData = await axios.get(url);
+    console.log(weatherData.data.data);
+    const weatherArray = weatherData.data.data.map((day) => new Forecast(day));
     response.status(200).send(weatherArray);
 
   } catch (error) {
